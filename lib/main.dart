2a6f321010bccedd7c3d78ab97cf54ac00b7e098
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2022-09-06 10:52:37 +0600

    Adds custom widgets to list movies

--- lib/main.dart
+++ lib/main.dart
@@ -30,12 +30,15 @@ class MoviesListing extends StatefulWidget {
 }
 
 class _MoviesListingState extends State<MoviesListing> {
-  var movies;
+  List<MovieModel> movies = <MovieModel>[];
 
   fetchMovies() async {
     var data = await MoviesProvider.getJson();
     setState(() {
-      movies = data['results'];
+      List<dynamic> results = data['results'];
+      results.forEach((element) {
+        movies.add(MovieModel.fromJson(element));
+      });
     });
   }
 
@@ -48,9 +51,7 @@ class _MoviesListingState extends State<MoviesListing> {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(8),
-            child: Text(
-              movies[index]['title'],
-            ),
+            child: MovieTile(movies, index),
           );
         },
       ),
@@ -66,5 +67,100 @@ class MoviesProvider {
         "http://api.themoviedb.org/3/discover/movie?api_key=$apiKey&sort_by=popularity.desc";
     final apiResponse = await http.get(apiEndPoint);
     return json.decode(apiResponse.body);
+  }
+}
+
+class MovieModel {
+  //Class fields for mapping to JSON properties
+  final int id;
+  final num popularity;
+  final int vote_count;
+  final bool video;
+  final String poster_path;
+  final String backdrop_path;
+  final bool adult;
+  final String original_language;
+  final String original_title;
+  final List<dynamic> genre_ids;
+  final String title;
+  final num vote_average;
+  final String overview;
+  final String release_date;
+
+  //Takes JSON formatted map, and returns `MovieModel` object.
+  MovieModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        popularity = json['popularity'],
+        vote_count = json['vote_count'],
+        video = json['video'],
+        poster_path = json['poster_path'],
+        adult = json['adult'],
+        original_language = json['original_language'],
+        original_title = json['original_title'],
+        genre_ids = json['genre_ids'],
+        title = json['title'],
+        vote_average = json['vote_average'],
+        overview = json['overview'],
+        release_date = json['release_date'],
+        backdrop_path = json['backdrop_path'];
+}
+
+class MovieTile extends StatelessWidget {
+  final List<MovieModel> movies;
+  final index;
+
+  const MovieTile(this.movies, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        children: [
+          movies[index].poster_path != null
+              ? Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery.of(context).size.height / 4,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey,
+                    image: DecorationImage(
+                      image: NetworkImage(MoviesProvider.imagePathPrefix +
+                          movies[index].poster_path),
+                      fit: BoxFit.cover,
+                    ),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 3.0,
+                        offset: Offset(1, 3),
+                      ),
+                    ],
+                  ),
+                )
+              : Container(),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              movies[index].title,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              movies[index].overview,
+            ),
+          ),
+          Divider(
+            color: Colors.grey.shade500,
+          ),
+        ],
+      ),
+    );
   }
 }
